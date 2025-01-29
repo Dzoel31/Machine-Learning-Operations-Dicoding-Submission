c58e8b55bf4808f7c35f3643df838685212aa732
Dzoel31 <dzulfikriadjmal@gmail.com>
2025-01-29 12:59:49 +0700

    init submission

---
+++ modules/sentence_sentiment_transform.py
@@ -0,0 +1,37 @@
+""" Transform module for Twitter Sentiment Analysis """
+
+import os
+import tensorflow as tf
+
+LABEL_KEY = "sentiment"
+FEATURE_KEY = "sentence"
+
+
+def transformed_name(key):
+    """Renaming transformed features
+    
+    Args:
+        key: str, feature key
+
+    Returns:
+        str, transformed feature key
+    """
+    return key + "_xf"
+
+def preprocessing_fn(inputs):
+    """Implement transormation for the input features
+
+    Args:
+        inputs: map from feature keys to raw features.
+
+    Returns:
+        outputs: map from feature keys to transformed features.
+    """
+
+    outputs = inputs.copy()
+
+    outputs[transformed_name(FEATURE_KEY)] = tf.strings.lower(inputs[FEATURE_KEY])
+
+    outputs[transformed_name(LABEL_KEY)] = tf.cast(inputs[LABEL_KEY], tf.int64)
+
+    return outputs
